10390fcd8914d54744b4e1a48704ced93c11ea79
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-16 07:56:41 -0400

    bug fixed and creating the game view

---
+++ guesser-multiplayer-game/Preview Content/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  guesser-multiplayer-game
+//
+//  Created by Marcelo Amaral Alves on 2023-10-16.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
