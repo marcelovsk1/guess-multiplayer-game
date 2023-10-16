3153ec0c0e2fd00ef367b537cd53a2901e5dc426
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-16 08:52:46 -0400

    creating the DrawingView

---
+++ guesser-multiplayer-game/Preview Content/DrawingView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawingView.swift
+//  guesser-multiplayer-game
+//
+//  Created by Marcelo Amaral Alves on 2023-10-16.
+//
+
+import SwiftUI
+
+struct DrawingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawingView()
+}
