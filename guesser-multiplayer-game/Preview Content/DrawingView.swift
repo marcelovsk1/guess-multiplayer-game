ec74b745eb2a2a4a6c2e81e73a1deffc9330a0aa
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-16 09:58:51 -0400

    first part of drawing view is done

--- guesser-multiplayer-game/Preview Content/DrawingView.swift
+++ guesser-multiplayer-game/Preview Content/DrawingView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 import PencilKit
 
 struct DrawingView: UIViewRepresentable {
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var matchManager: MatchManager
+
+        init(matchManager: MatchManager) {
+            self.matchManager = matchManager
+        }
+
+        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+            // Implemente a lógica apropriada aqui, se necessário.
+        }
+    }
+             
     var canvasView = PKCanvasView()
     
     @ObservedObject var matchManager: MatchManager
-    @Binding var eraserEnabled: Bool
+        @Binding var eraserEnabled: Bool
+
+        func makeUIView(context: Context) -> PKCanvasView {
+            let canvasView = PKCanvasView()
+            canvasView.drawingPolicy = .anyInput
+            canvasView.tool = PKInkingTool(.pen, color: .black, width: 5)
+            canvasView.delegate = context.coordinator
+            canvasView.isUserInteractionEnabled = matchManager.currentlyDrawing
+
+            return canvasView
+        }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(matchManager: matchManager)
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        //TODO: Handle varius updates
+        
+        canvasView.tool = eraserEnabled ?
+        PKEraserTool(.vector) : PKInkingTool(.pen, color: .black, width: 5)
+    }
 }
     
-@State static var eraser = false
-#Preview {
-    DrawingView(matchManager: MatchManager(), eraserEnabled: <#Binding<Bool>#>)
+//#Preview {
+//    @State var eraser = false
+//    DrawingView(matchManager: MatchManager(), eraserEnabled: $eraser)
+//}
+
+struct DrawingView_Previews: PreviewProvider {
+    @State static var eraser = false
+    static var previews: some View {
+        DrawingView(matchManager: MatchManager(),
+        eraserEnabled: $eraser)
+    }
 }
