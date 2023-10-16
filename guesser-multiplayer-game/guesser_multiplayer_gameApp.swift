7270a434aebf37d8d0b16063565087d78fd98838
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-15 23:30:11 -0400

    Initial Commit

---
+++ guesser-multiplayer-game/guesser_multiplayer_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  guesser_multiplayer_gameApp.swift
+//  guesser-multiplayer-game
+//
+//  Created by Marcelo Amaral Alves on 2023-10-15.
+//
+
+import SwiftUI
+
+@main
+struct guesser_multiplayer_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
