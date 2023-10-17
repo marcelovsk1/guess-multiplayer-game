356528a471f2d3f4fe0bd54fbe75cd61dd1af826
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-17 15:57:40 -0400

    timer is added

---
+++ guesser-multiplayer-game/Preview Content/MatchManager+GKMatchDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  MatchManager+GKMatchDelegate.swift
+//  guesser-multiplayer-game
+//
+//  Created by Marcelo Amaral Alves on 2023-10-17.
+//
+
+import Foundation
---
+++ guesser-multiplayer-game/Preview Content/MatchManager+GKMatchMakerViewControllerDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  MatchManager+GKMatchMakerViewControllerDelegate.swift
+//  guesser-multiplayer-game
+//
+//  Created by Marcelo Amaral Alves on 2023-10-17.
+//
+
+import Foundation
