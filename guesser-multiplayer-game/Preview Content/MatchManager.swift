230520d744365d731b21b8ec6e660518e1c74e0a
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-17 14:16:10 -0400

    implementing GameKit and PencilKit in MatchManager

--- guesser-multiplayer-game/Preview Content/MatchManager.swift
+++ guesser-multiplayer-game/Preview Content/MatchManager.swift
@@ -6,17 +6,54 @@
 //
 
 import Foundation
+import GameKit
+import PencilKit
 
 class MatchManager: ObservableObject {
     @Published var inGame = false
     @Published var inGameOver = false
     @Published var authenticaitonState = PlayerAuthState.authenticating
     
-    @Published var currentlyDrawing = true 
+    @Published var currentlyDrawing = true
     @Published var drawPrompt = "Dishwasher"
     @Published var pastGuesses = [PastGuess]()
     
     @Published var score = 0
     @Published var remainingTime = maxTimeRemaining
     
+    var match: GKMatch?
+    var otherPlayer: GKPlayer?
+    var localPLayer = GKLocalPlayer.local
+    
+    var playerUUIDKey = UUID().uuidString
+    
+    var rootViewController: UIViewController? {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return windowScene?.windows.first?.rootViewController
+    }
+    
+    func authenticateUser() {
+        GKLocalPlayer.local.authenticateHandler = { [self] vc, e in
+            if let viewController = vc {
+                rootViewController?.present(viewController, animated: true)
+                return
+            }
+            
+            if let error = e {
+                authenticaitonState = .error
+                print(error.localizedDescription)
+                
+                return
+            }
+            if localPLayer.isAuthenticated {
+                if localPLayer.isMultiplayerGamingRestricted {
+                    authenticaitonState = .restricted
+                } else {
+                    authenticaitonState = .authenticated
+                }
+            } else {
+                authenticaitonState = .unauthenticated
+            }
+        }
+    }
 }
