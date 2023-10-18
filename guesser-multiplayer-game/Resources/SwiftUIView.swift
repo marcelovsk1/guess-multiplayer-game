fda8ed9018e235fbb916ad5522154fc3b923ef79
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-18 15:34:58 -0400

    fixing some bugs

---
+++ guesser-multiplayer-game/Resources/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  guesser-multiplayer-game
+//
+//  Created by Marcelo Amaral Alves on 2023-10-18.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
